babbcb0f278370408353a957baa65df12abfba6d
Swifter <swifter1243@gmail.com>
2024-09-01 18:31:39 -0400

    ok I figured out how to make a wallpaper

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const canvas = document.getElementById("canvas")
 const context = canvas.getContext("2d")
 
-// context.clearRect(0, 0, 1920, 1080)
-// context.fillText('balls', 0, 0)
+window.onload = setup
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.style.width = window.innerWidth + "px";
+    canvas.style.height = window.innerHeight + "px";
+}
+
+function setup() {
+    resizeCanvas()
+
+    queueNextFrame()
+}
+
+function queueNextFrame() {
+    window.requestAnimationFrame(draw)
+}
+
+function draw() {
+    queueNextFrame()
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillText('balls', 300, 300)
+}
